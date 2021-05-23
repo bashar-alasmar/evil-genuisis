81a29faca8e40d8298aa5b5f7fe64f6b0f8526ba
bashar-alasmar <basharalasmar96@gmail.com>
2021-05-23 12:27:25 +0300

    demo

---
+++ js/app.js
@@ -0,0 +1,44 @@
+/*
+Multi line comment
+*/
+
+// single line comment 
+
+
+//Variables
+// its something to store values! 
+//Const & Let => ECMA6
+// let userName = 'Anood';
+
+// let userPassword;
+// declaring variable 
+
+// let user = 'Ahmad';
+// declare and initializing
+// userName => camelCase
+// let & const
+// let user = 'Bashar';
+// user = 'Mohammad';
+// user = 23214123;
+// console.log(user);
+
+// const url = 'www.google.com';
+// url = 23;
+
+let userName = prompt('Whats your name?');
+// console.log(userName);
+// anything we retreive from a prompt its a string!
+let userAge = prompt('Whats your age?');
+// console.log(userAge);
+
+let favMovie = prompt('Whats your fav movie?');
+// console.log(favMovie);
+
+
+
+// alert('Your Name is ' + userName + ' and your age is ' + userAge + 'your favourite movie is ' + favMovie)
+// Concatination 
+
+alert(`Your Name is ${userName} and your age is ${userAge} and your fav movie is ${favMovie}`);
+//string interpolation
+ 
